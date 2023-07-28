4f52dd7e4e42b42298879221691b4c93886cf201
Amanda Young <botallytonkers@gmail.com>
2023-07-28 15:43:32 -0700

    todolist wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,7 @@
  import React from 'react'
 import axios from 'axios'
 import Form from './Form'
+import TodoList from './TodoList'
 
 const URL = 'http://localhost:9000/api/todos'
 
@@ -68,18 +69,12 @@ export default class App extends React.Component {
     return (
     <div>
       <div id='error'>Error: {this.state.error}</div>
-      <div id='todos'>
-        <h2>Todos:</h2>
-        {
-          this.state.todos.reduce((acc, td) => {
-            if (this.state.displayCompleteds || !td.completed) return acc.concat(
-              <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✔️ ' : ''}</div>
-            )
-            return acc
-          }, [])
-        }
+      <TodoList
+       todos={this.state.todos}
+       displayCompleteds={this.state.displayCompleteds}
+       toggleCompleted={this.toggleCompleted}
 
-      </div>
+      />
      <Form
       onTodoFormSubmit={this.onTodoFormSubmit}
       todoNameInput={this.state.todoNameInput}
